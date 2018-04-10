99a3774c5a27a62d9f7cfcbb17432f562fb3485d
pdutie94 <ptiencntt94@gmail.com>
2018-04-11 01:23:23 +0700

    Fix Bootstrap webkit error

--- resources/views/admin/layouts/index.blade.php
+++ resources/views/admin/layouts/index.blade.php
@@ -41,11 +41,11 @@
 <!-- Le javascript
 ================================================== --> 
 <!-- Placed at the end of the document so the pages load faster --> 
-<script src="js/jquery-1.8.0.min.js"></script> 
+<script src="js/jquery-1.12.4.min.js"></script> 
 <script src="js/excanvas.min.js"></script> 
 <!--<script src="js/chart.min.js" type="text/javascript"></script>-->
-<script src="js/bootstrap.js"></script>
-<script language="javascript" type="text/javascript" src="js/full-calendar/fullcalendar.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--<script language="javascript" type="text/javascript" src="js/full-calendar/fullcalendar.min.js"></script>-->
  
 <script src="js/base.js"></script>
 <script>
